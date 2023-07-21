c3291cce37dae81c885cc1a6e1baf4ce01445724
therealemissions <me@liammartin.site>
2023-07-21 17:22:05 +0100

    updated

--- database/src/Database/MongoDB/ModelBuilder.d.ts
+++ database/src/Database/MongoDB/ModelBuilder.d.ts
@@ -1,8 +1,8 @@
 import Mongoose from "mongoose";
 import { Schema } from "../../Types/SchemaType.js";
 export declare class ModelBuilder {
-    static models: Map<string, Mongoose.Model<Schema, {}, {}, {}, Mongoose.Document<unknown, {}, Schema> & Omit<Schema & Required<{
+    static models: Map<string, Mongoose.Model<Schema, {}, {}, {}, Mongoose.Document<unknown, {}, Schema> & Schema & Required<{
         _id: string;
-    }>, never>, any>>;
+    }>, any>>;
     static build(): void;
 }
